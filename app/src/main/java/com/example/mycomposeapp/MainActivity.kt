67488c28e0d722b3b64836b62e693e84cd6fcd58
Chauhan, Renu <Renu.Chauhan@tabcorp.com.au>
2025-08-20 12:41:30 +0530

    implement fastlane and CI/CD

--- app/src/main/java/com/example/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapp/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycomposeapp.screens.TweetsScreen
 import com.example.mycomposeapp.ui.theme.MyApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.compose.material.*
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
@@ -26,7 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme{
                 Scaffold(topBar =  { TopAppBar (
-                    title = {Text(text = "Tweetsy")})}, containerColor = Color.Black, contentColor = Color.White)
+                    title = {Text(text = "Tweest Tweest")})}, containerColor = Color.Black, contentColor = Color.White)
                  {
                      Box(modifier = Modifier.padding(it)){
                          TweetsScreen()
